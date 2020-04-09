541602a93632569f3b37feae40d3f0162586bb5a
gabriel <gabrieldausque.pro@gmail.com>
2020-04-09 12:41:29 +0200

    Add npm packages creation - round 3

--- src/services/topic/index.ts
+++ src/services/topic/index.ts
@@ -1,19 +1,9 @@
-import {TopicMessage as TM} from './datas/TopicMessage';
-import {ITopicClient as ITC} from './interfaces/ITopicClient';
-import {TopicHandler as TH} from "./TopicHandler";
-import {BaseTopicClient as BTC} from "./BaseTopicClient";
-import {MemoryTopicServiceClient as MTSC} from "./MemoryTopicServiceClient";
-import {SocketIOTopicServiceClient as SIOTSC} from "./SocketIOTopicServiceClient";
-import {TopicService as TS} from "./TopicService";
-import {TopicHandlerFunction as THF} from "./interfaces/ITopicClient";
+export * from './datas/TopicMessage';
+export * from './interfaces/ITopicClient';
+export * from './BaseTopicClient';
+export * from './MemoryTopicServiceClient';
+export * from './SocketIOTopicServiceClient';
+export * from './TopicHandler';
+export * from './TopicService';
 
-export namespace Hermes {
-  export type TopicMessage = TM;
-  export type ITopicClient = ITC;
-  export type TopicHandler = TH;
-  export type BaseTopicClient = BTC;
-  export type MemoryTopicServiceClient = MTSC;
-  export type SocketIOTopicServiceClient = SIOTSC;
-  export type TopicService = TS;
-  export type TopicHandlerFunction = THF;
-}
+
