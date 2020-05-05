d1537c7140b7c6068bedce632200200c1d029935
gabriel <gabrieldausque.pro@gmail.com>
2020-05-05 11:00:44 +0200

    Update version for package regeneration

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,10 +1,5 @@
 import { Application as ExpressFeathers } from '@feathersjs/express';
-import {BackEndService} from "./services/backend/BackEndService";
-import {TopicService} from "./services/topic/TopicService";
-import {MoleculeLoader} from "./services/moleculeloader/moleculeLoader";
 import {Platform} from "./platform/Platform";
-import {Project} from "./services/project/project.class";
-import {Logging} from "./services/logging/logging.class";
 
 // A mapping of service names to types. Will be extended in service files.
 export interface ServiceTypes {}
